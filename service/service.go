1437ce19bd9fd0324ba3ed2f5862fbf5c1074b31
richardsahvic <richsegar1208@yahoo.com>
2019-02-16 18:47:22 +0800

    ON PROGRESS

--- service/service.go
+++ service/service.go
@@ -9,6 +9,7 @@ type UserService interface {
 	Login(username string, password string) (string, error)
 	ChangePassword(token string, password string, newPassword string) (bool, error)
 	AllGenre() (struct)
+	AllInstrument() (struct)
 }
 
 var User UserService
